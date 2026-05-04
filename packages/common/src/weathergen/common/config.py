0e3a831e0771555867f2cb3509bd4f1ade0e4adc
Florian Scheidl <f.scheidl@fz-juelich.de>
2026-05-04 13:35:12 +0200

    Minimal profiling setup wip

--- packages/common/src/weathergen/common/config.py
+++ packages/common/src/weathergen/common/config.py
@@ -671,6 +671,11 @@ def get_path_run(config: Config) -> Path:
     return _get_shared_wg_path() / "results" / get_run_id_from_config(config)
 
 
+def get_path_profiler(config: Config) -> Path:
+    """Get the path for storing profiling logs."""
+    return _get_shared_wg_path() / "profiler_logs" / get_run_id_from_config(config)
+
+
 def get_path_model(config: Config | None = None, run_id: str | None = None) -> Path:
     """Get the current runs model_path for storing model checkpoints."""
     if config or run_id:
