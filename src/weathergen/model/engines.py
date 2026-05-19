dfa0012a935d06a460a804093b1e6605b179057e
Florian Scheidl <f.scheidl@fz-juelich.de>
2026-05-19 16:32:05 +0200

    restore

--- src/weathergen/model/engines.py
+++ src/weathergen/model/engines.py
@@ -29,7 +29,6 @@ from weathergen.model.embeddings import (
     StreamEmbedTransformer,
 )
 from weathergen.model.layers import MLP
-from weathergen.model.norms import LayerNorm
 from weathergen.model.utils import ActivationFactory
 from weathergen.utils.utils import get_dtype
 
@@ -69,7 +68,6 @@ class EmbeddingEngine(torch.nn.Module):
                     num_blocks=si["embed"]["num_blocks"],
                     num_heads=si["embed"]["num_heads"],
                     dropout_rate=self.cf.embed_dropout_rate,
-                    with_flash=self.cf.with_flash_attention,
                     norm_type=self.cf.norm_type,
                     unembed_mode=self.cf.embed_unembed_mode,
                     stream_name=stream_name,
@@ -521,7 +519,7 @@ class GlobalAssimilationEngine(torch.nn.Module):
             )
         if self.cf.get("ae_global_trailing_layer_norm", False):
             self.ae_global_blocks.append(
-                LayerNorm(self.cf.ae_global_dim_embed)
+                torch.nn.LayerNorm(self.cf.ae_global_dim_embed, elementwise_affine=False)
             )
 
     def forward(self, tokens, coords=None):
@@ -599,7 +597,7 @@ class ForecastingEngine(torch.nn.Module):
                 # Optionally, add LayerNorm after i-th layer
                 if i in self.cf.get("fe_layer_norm_after_blocks", []):
                     self.fe_blocks.append(
-                        LayerNorm(self.cf.ae_global_dim_embed)
+                        torch.nn.LayerNorm(self.cf.ae_global_dim_embed, elementwise_affine=False)
                     )
 
         def init_weights_final(m):
@@ -848,8 +846,8 @@ class TargetPredictionEngine(nn.Module):
             "attention_dtype": get_dtype(self.cf.attention_dtype),
         }
         self.tte = nn.ModuleList()
-        self.output_in_norm = LayerNorm(self.dims_embed[0])
-        self.latent_in_norm = LayerNorm(self.cf.ae_global_dim_embed)
+        self.output_in_norm = nn.LayerNorm(self.dims_embed[0])
+        self.latent_in_norm = nn.LayerNorm(self.cf.ae_global_dim_embed)
         self.final_norm = nn.Identity()  # nn.RMSNorm(self.dims_embed[-1])
         self.dropout = nn.Dropout(0.2)
         self.pos_embed = nn.Parameter(torch.zeros(1, 9, self.cf.ae_global_dim_embed))
