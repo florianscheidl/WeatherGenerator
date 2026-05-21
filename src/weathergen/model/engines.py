f105acc3477cbaef4b030461d00da6db1566c6ec
Florian Scheidl <f.scheidl@fz-juelich.de>
2026-05-21 09:41:46 +0200

    Try not checkpointing the target-token-engine decover paths

--- src/weathergen/model/engines.py
+++ src/weathergen/model/engines.py
@@ -771,19 +771,14 @@ class TargetPredictionEngineClassic(nn.Module):
 
         for ib, block in enumerate(self.tte):
             if self.cf.pred_self_attention and ib % 3 == 1:
-                tc_tokens = checkpoint(
-                    block, tc_tokens, tcs_lens, tcs_aux, use_reentrant=False, debug=True
-                )
+                tc_tokens = block(tc_tokens, tcs_lens, tcs_aux)
             else:
-                tc_tokens = checkpoint(
-                    block,
+                tc_tokens = block(
                     tc_tokens,
                     tokens_stream,
                     tcs_lens,
                     tokens_lens,
                     tcs_aux,
-                    use_reentrant=False,
-                    debug=True,
                 )
         return tc_tokens
 
@@ -936,35 +931,26 @@ class TargetPredictionEngine(nn.Module):
         )
         for layer in self.tte:
             if isinstance(layer, OriginalPredictionBlock):
-                output = checkpoint(
-                    layer,
+                output = layer(
                     latent=latent.flatten(0, 1),
                     output=output,
                     coords=coordinates,
                     latent_lens=latent_lens,
                     output_lens=output_lens,
-                    use_reentrant=False,
-                    debug=True,
                 )
             elif isinstance(layer, CrossAttentionBlock):
-                output = checkpoint(
-                    layer,
+                output = layer(
                     x=output,
                     x_kv=latent.flatten(0, 1),
                     x_lens=output_lens,
                     aux=latent[:, 0],
                     x_kv_lens=latent_lens,
-                    use_reentrant=False,
-                    debug=True,
                 )
             else:
-                output = checkpoint(
-                    layer,
+                output = layer(
                     x=output,
                     x_lens=output_lens,
                     aux=latent[:, 0],
-                    use_reentrant=False,
-                    debug=True,
                 )
         output = (
             self.final_norm(output)
