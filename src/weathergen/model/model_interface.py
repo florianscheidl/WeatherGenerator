9ce740a0da2684b44390d9df096628cb47aaad23
Florian Scheidl <f.scheidl@fz-juelich.de>
2026-05-27 10:04:57 +0200

    Undo embedding engine wrapping

--- src/weathergen/model/model_interface.py
+++ src/weathergen/model/model_interface.py
@@ -142,12 +142,7 @@ def init_model_and_shard(
             modules_to_shard,
             debug=checkpoint_debug,
         )
-
-        for stream_embed_list in model.encoder.embed_engine.embeds.values():
-            for module in stream_embed_list.modules():
-                if isinstance(module, modules_to_shard):
-                    fully_shard(module, **fsdp_kwargs)
-
+        
         for module in model.encoder.ae_local_engine.ae_local_blocks.modules():
             if isinstance(module, modules_to_shard):
                 fully_shard(module, **fsdp_kwargs)
