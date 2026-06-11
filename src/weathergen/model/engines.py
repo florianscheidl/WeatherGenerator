ee4384c4d2e162a34202dd1e52e671a1814403a5
Florian Scheidl <f.scheidl@fz-juelich.de>
2026-06-11 16:33:46 +0200

    try applying checkpointing at a finer granularity

--- src/weathergen/model/engines.py
+++ src/weathergen/model/engines.py
@@ -240,7 +240,7 @@ class LocalAssimilationEngine(torch.nn.Module):
                 )
             )
 
-    def forward(self, tokens_c, cell_lens_c, use_reentrant):
+    def forward(self, tokens_c, cell_lens_c):
         for block in self.ae_local_blocks:
             tokens_c = block(tokens_c, cell_lens_c)
         return tokens_c
